4cbb3ec6f2ca51026e851cc0b8f35d8a408b1676
David Soundar <52380734+davidsoundar@users.noreply.github.com>
2024-04-08 23:10:14 -0500

    Update sedan.cs

--- sedan.cs
+++ sedan.cs
@@ -5,18 +5,14 @@ public partial class sedan : Node2D
 {
 	private AnimationPlayer animationPlayer;
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// Get a reference to the AnimationPlayer node
 		animationPlayer = GetNode<AnimationPlayer>("SedanVintage/sedan");
 
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		// Play the "sedan" animation in the AnimationPlayer
 		animationPlayer.Play("sedan");
 	}
 }
